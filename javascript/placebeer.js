629a66d833e96b2c4b79756b15ce89819f13bfae
Christian Lønaas <christian.lonaas@discombobulator.org>
2019-04-10 22:18:13 +0200

    Remove 'srcset' attribute
    Should probably find a way to replace these as well.
    For now, remove it, allowing 'src' to be loaded.

--- javascript/placebeer.js
+++ javascript/placebeer.js
@@ -3,6 +3,7 @@ for(i in document.images) {
     h = document.images[i].clientHeight;
     
     if((w > 30) && (h > 30) && (w < 1921) && (h < 1921)) {
+      document.images[i].srcset = '';
       document.images[i].src = 'https://placebeer.com/' + w + '/' + h;
     }
 }
